56261043080b4c945286e1cdc682ec4177f6988a
Janpstrunn <janpstrunn@disroot.org>
2025-05-16 16:26:04 -0300

    feat(waybar): Right clicking the client module will display the current workspace

--- waybar/scripts/clients.sh
+++ waybar/scripts/clients.sh
@@ -1,14 +1,57 @@
 #!/usr/bin/env bash
 
-total_clients=$(hyprctl clients -j | jq -r ".[].title" | wc -l)
+# Known bug:
+# If a window name has "&", the tooltip won't show up
 
-client_names=$(hyprctl clients -j | jq -r '.[] | "\(.workspace.id) | \(.title)"')
+FLAG=$1
+FLAG_CACHE=$HOME/.cache/waybar-client
 
-tooltip="Running Clients: $total_clients"
-tooltip+="\n\nClients:"
+tooltip="LMB: Clients"
+tooltip+="\nRMB: Workspace"
 
-while IFS= read -r client; do
-  tooltip+="\n$client"
-done <<<"$client_names"
+function get_clients() {
+  total_clients=$(hyprctl clients -j | jq -r ".[].title" | wc -l)
+  client_names=$(hyprctl clients -j | jq -r '.[] | "\(.workspace.id) | \(.title)"')
 
-echo "{\"text\": \"󰣆  $total_clients\", \"tooltip\": \"$tooltip\"}"
+  tooltip+="\n\nRunning Clients: $total_clients"
+  tooltip+="\n\nClients:"
+
+  while IFS= read -r client; do
+    tooltip+="\n$client"
+  done <<<"$client_names"
+
+  text=$total_clients
+  icon=󰣆
+}
+
+function get_workspace() {
+  workspace=$(hyprctl activeworkspace | sed -n 's/.*(\([^)]*\)).*/\1/p')
+  tooltip+="\n\nCurrent Workspace: $workspace"
+  text=$workspace
+  icon=
+}
+
+function save_flag() {
+  echo "$FLAG" >"$FLAG_CACHE"
+}
+
+function _return() {
+  echo "{\"text\": \"$icon  $text\", \"tooltip\": \"$tooltip\"}"
+}
+
+function main() {
+  if [ -n "$FLAG" ]; then
+    save_flag
+  fi
+  case "$(cat "$FLAG_CACHE")" in
+  workspace)
+    get_workspace
+    ;;
+  *)
+    get_clients
+    ;;
+  esac
+  _return
+}
+
+main "$FLAG"
